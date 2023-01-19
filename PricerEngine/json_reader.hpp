d13069fc222ff2193491aadad03c9cf7c3dd380d
unknown <emmalito.delar@grenoble-inp.org>
2023-01-19 10:47:08 +0100

    suppression pricer prof

--- PricerEngine/json_reader.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <nlohmann/json.hpp>
-#include "pnl/pnl_vector.h"
-#include "pnl/pnl_matrix.h"
-
-void from_json(const nlohmann::json &j, PnlVect *&vect);
-void from_json(const nlohmann::json &j, PnlMat *&mat);
